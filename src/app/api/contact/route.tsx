11133d051b085972587ab1b3f735a8aafe283271
Inaki Ulrich <inaki.ulrich@gmail.com>
2025-04-02 16:29:21 -0700

    another try heroku ionos

--- src/app/api/contact/route.tsx
+++ src/app/api/contact/route.tsx
@@ -16,8 +16,8 @@ export async function POST(
 
             const transporter = nodemailer.createTransport({
                 host: process.env.IONOS_HOST,
-                port: Number(process.env.PORT) || 465,
-                secure: true,
+                port: parseInt(process.env.SMTP_PORT || '587'),
+                secure: process.env.SMTP_SECURE === 'true',
                 auth: {
                     user: process.env.IONOS_USER,
                     pass: process.env.IONOS_PASSWORD,
